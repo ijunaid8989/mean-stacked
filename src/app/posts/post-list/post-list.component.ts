9a73a3d8af2c5aafa726ce2bea45349f75c667bd
Junaid Farooq <junaid@evercam.io>
2019-07-24 11:46:22 +0500

    Added PostsService as Injectable.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from "@angular/core";
 
 import { Post } from "../post.model";
+import { PostsService } from "../posts.service";
 
 @Component({
   selector: "app-post-list",
@@ -14,4 +15,7 @@ export class PostListComponent {
   //   {title: "Third Post", content: "This is Third post"}
   // ]
   @Input() posts: Post[] = [];
+
+  constructor(public postsService: PostsService) {
+  }
 }
