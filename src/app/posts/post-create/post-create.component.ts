f97cee4946c5a2314ffce15ed626c4104097d113
Junaid Farooq <junaid@evercam.io>
2019-07-26 20:37:12 +0500

    Start using Post Service.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
-import { Post } from "../post.model"
+import { PostsService } from "../posts.service";
 
 @Component({
   selector: "app-post-create",
@@ -9,18 +9,13 @@ import { Post } from "../post.model"
   styleUrls: ["./post-create.component.css"]
 })
 export class PostCreateComponent {
-  title = "";
-  content = "";
-  @Output() postCreated = new EventEmitter<Post>();
+  constructor(public postsService: PostsService) {}
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content
-    };
-    this.postCreated.emit(post);
+
+    this.postsService.addPost(form.value.title, form.value.content)
   }
 }
